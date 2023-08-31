dc9080b928057022004c058516dc7148addc6564
MackGg <MackDing@users.noreply.github.com>
2023-08-31 18:32:27 +0800

    Update linux_autoGreen.sh

--- linux_autoGreen.sh
+++ linux_autoGreen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# git remote add origin https://github.com/MackDing/AutoGreen.git
 # crontab -e  
 # add fileName to Cron list (ex. 1 */2 * * * /data/mack/github/autoGreen/linux_autoGreen.sh)
 # 1 */2 * * * /data/mack/github/autoGreen/linux_autoGreen.sh
@@ -8,7 +8,5 @@ cd /data/mack/github/autoGreen/
 echo "🕶️Last updated: $(TZ=Asia/Singapore date '+%Y-%m-%d %H:%M:%S %Z')" >> README.md
 git add README.md
 git commit -m "A commit a day keeps the girlfriend away."
-# git remote add origin https://github.com/MackDing/AutoGreen.git
 git push origin master
 echo I'M Stronger..
-
