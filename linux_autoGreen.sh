37070e1697849b1de6e26a807407d957f7d9feea
MackGg <MackDing@users.noreply.github.com>
2023-08-31 19:11:34 +0800

    Update linux_autoGreen.sh

--- linux_autoGreen.sh
+++ linux_autoGreen.sh
@@ -4,8 +4,8 @@
 # add fileName to Cron list (ex. 1 */2 * * * /data/mack/github/autoGreen/linux_autoGreen.sh)
 # 1 */2 * * * /data/mack/github/autoGreen/linux_autoGreen.sh
 
-cd /data/mack/github/autoGreen/
-echo "🕶️Last updated: $(TZ=Asia/Singapore date '+%Y-%m-%d %H:%M:%S %Z')" >> README.md
+cd "$(dirname "$0")"
+echo "🕶️Last updated: $(TZ=Asia/Singapore date '+%Y-%m-%d %H:%M:%S %Z')" > README.md
 git add README.md
 git commit -m "A commit a day keeps the girlfriend away."
 git push origin master
