122810e010b7c09f2a41fc53be193b0944ebaaf6
Mack <mack_ding@outlook.com>
2023-08-31 18:05:55 +0800

    A commit a day keeps the girlfriend away.

--- linux_autoGreen.sh
+++ linux_autoGreen.sh
@@ -8,7 +8,7 @@ cd /data/mack/github/autoGreen/
 echo "🕶️Last updated: $(TZ=Asia/Singapore date '+%Y-%m-%d %H:%M:%S %Z')" >> README.md
 git add README.md
 git commit -m "A commit a day keeps the girlfriend away."
-git remote add origin https://github.com/MackDing/AutoGreen.git
+# git remote add origin https://github.com/MackDing/AutoGreen.git
 git push origin master
-echo I'M Stronger..
+echo I'M Stronger...
 
